286af6f952232152b666fe5ccc84f17537c0ed15
Lutsman_win <333333y@gmail.com>
2019-05-02 01:55:21 +0300

    ranges sliders

--- src/scripts/app/rangesSlider.js
+++ src/scripts/app/rangesSlider.js
@@ -1,12 +1,48 @@
-$(() => {
-    const $simpleInput = $('input[type="range"]');
-    const hadleInput = ($target) => (e) => $target.text(e.target.value);
+import noUiSlider from 'nouislider';
+import 'nouislider/distribute/nouislider.css';
 
-    $simpleInput.each(function () {
-        const $input = $(this);
-        const $target = $($input.attr('data-target'));
-        const handler = hadleInput($target);
+const simpleRangeInput = document.querySelectorAll('input[type="range"]');
+const handleSlideWrapper = (target, input) => (values, handle) => {
+    const val = parseInt(values[handle]);
 
-        $input.on('input', handler);
-    });
-});
+    target.value = val;
+    input.value = val;
+};
+const handleInputWrapper = (slider, input, min, max) => (e) => {
+    const target = e.target;
+    let val = parseInt(target.value);
+
+    if (min !== undefined && val < min) {
+        val = min;
+    }
+
+    if (max !== undefined && val > max) {
+        val = max;
+    }
+
+    slider.noUiSlider.set(val);
+    input.value = val;
+};
+
+for (const input of simpleRangeInput) {
+    const options = {
+        start: input.value,
+        step: parseInt(input.getAttribute('step')) || 1,
+        range: {
+            'min': parseInt(input.getAttribute('min')),
+            'max': parseInt(input.getAttribute('max')),
+        },
+    };
+    const parent = input.parentNode;
+    const slider = document.createElement('div');
+    const target = document.querySelector(input.getAttribute('data-target'));
+    const handleInput = handleInputWrapper(slider, input, options.range.min, options.range.max);
+    const handleSlide = handleSlideWrapper(target, input);
+
+    input.style.display = 'none';
+    parent.insertBefore(slider, input);
+    noUiSlider.create(slider, options);
+
+    slider.noUiSlider.on('slide', handleSlide);
+    target.addEventListener('input', handleInput);
+}
